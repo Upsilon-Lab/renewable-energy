c545a7df57dafa19d027f4fc4f875f704879886b
Adison Guo <93299361+AdaTastic@users.noreply.github.com>
2022-03-03 23:53:16 -0800

    backup real quick testing code

--- Arduino/trackyCode/servotest.ino
+++ Arduino/trackyCode/servotest.ino
@@ -1,4 +1,5 @@
-#include<Servo.h>
+#include <Servo.h>
+#include <math.h>
 Servo botServo; // Servo that is at the base
 Servo topServo; // Servo at the the top 
 
@@ -15,8 +16,9 @@ float pin1 = 0; // Top right
 float pin2 = 0; // Bottom left
 float pin3 = 0; // Bottom right
 
-const int increment = 1;
+float inc = 0;
 const int threshold = 1500;
+float diff = 0;
 
 void loop() {
   pin0 = ohmRead(0);
@@ -25,43 +27,50 @@ void loop() {
   pin3 = ohmRead(3);
 
   //top to bottom
-  if (abs((pin0 + pin1) - (pin2 + pin3)) < threshold ){
-    theta = theta;
-  } 
-  else if ((pin0 + pin1) > (pin2 + pin3)){
-    theta = theta - increment;
+
+  diff = (pin0 + pin1) - (pin2 + pin3);
+  inc = calcInc(diff);
+  
+  if (abs(diff) < threshold ){}
+  else if (diff > 0){
+    theta = theta - inc;
+    if (theta < 10){
+      theta = 10;
+    }
+    topServo.write(theta);
   } else {
-    theta = theta + increment;
+    theta = theta + inc;
+    if ( theta > 90 ) {
+      theta = 90;
+    }
+    topServo.write(theta);
   }
-  if (theta < 10){
-    theta = 10;
-  } else if ( theta > 90 ) {
-    theta = 90;
-  }
-  topServo.write(theta);
+  
   
   pin0 = ohmRead(0);
   pin1 = ohmRead(1);
   pin2 = ohmRead(2);
   pin3 = ohmRead(3);
   //side to side
-  if (abs((pin0 + pin2) - (pin1 + pin3)) < threshold){
-    phi = phi;
-  }
-  else if ((pin0 + pin2) > (pin1 + pin3)){
-    phi = phi + increment;
+
+  diff = (pin0 + pin2) - (pin1 + pin3);
+  inc = calcInc(diff);
+  
+  if (abs(diff) < threshold){}
+  else if (diff > 0){
+    phi = phi + inc;
+    if (phi > 180){
+      phi = 180;
+    }
+    botServo.write(phi);
   } else {
-    phi = phi - increment;
-  }
-  if (phi < 0){
-    phi = 0;
-  } else if (phi > 180){
-    phi = 180;
-  }
-  
-  botServo.write(phi);
-  
-  delay(30);
+    phi = phi - inc;
+      if (phi < 0){
+      phi = 0;
+    }
+    botServo.write(phi);
+  }  
+  delay(90);
 }
 
 const int   Vin = 5;
@@ -83,4 +92,16 @@ float ohmRead(int pin){
   else {
     return 0;
   }
+}
+
+const int minInc = 0.5;
+const int maxInc = 2;
+float calcInc(float diff){
+  inc = abs(diff)/(threshold);
+  if (inc > maxInc){
+    inc = maxInc;
+  } else if (inc < minInc){
+    inc = minInc;
+  }
+  return inc;
 }
