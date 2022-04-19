b28760cd079ecc56aeb0d72f6eb77231c66c7fec
David Gotler <58965004+davhgotler@users.noreply.github.com>
2022-04-19 16:34:56 -0700

    Calculated power gain from tracking

--- SolarModel/SolarModel.py
+++ SolarModel/SolarModel.py
@@ -1,3 +1,4 @@
+from traceback import print_tb
 import numpy as np
 import pandas as pd
 import solarpy as sp
@@ -34,10 +35,22 @@ G = G[G.mean(axis=1)!=0]
 s = 13.23 * 8.07 * .0254**2
 # Efficiency of 20%
 eff = 10/(G.values.max()*s)
-print(eff)
+print('Efficiency needed to reach max 10W for tracking: {:.2f}%'.format(eff*100))
 P = G*s*eff
 
+# Calculate the power gain from tracking w.r.t. the other directions
+
+
+P_diff = [P['Tracking'] - P[label] for label in P.keys()[0:-1]]
+P_diff = pd.concat(P_diff,axis=1,keys=P.keys()[0:-1])
+print('Average gain in power from tracking:')
+print(P_diff.mean())
+
 # plotting
+# --------
+fig_dir = 'figures/'
+
+# Plot solar irradiance and power output of the solar panel
 fig,axs = plt.subplots(1,2,figsize=(10,4))
 G.plot(ax=axs[0]).legend(loc='upper right')
 axs[0].set_title('Daily Solar Irradiance')
@@ -48,5 +61,12 @@ axs[1].set_title('Solar Panel {:.2f}% Efficiency'.format(eff*100))
 axs[1].set_xlabel('$t\ [minutes]$')
 axs[1].set_ylabel('$P\ [W]$')
 
-fig_dir = 'figures/'
 plt.savefig(fig_dir+'solar_irradience_power_{:.2f}eff.png'.format(eff*100))
+
+fig,ax = plt.subplots(figsize=(6,4))
+P_diff.plot(ax=ax).legend(loc='upper right')
+ax.set_title('Power Gain From Tracking')
+ax.set_xlabel('$t\ [minutes]$')
+ax.set_ylabel('$\Delta P\ [W]$')
+
+plt.savefig(fig_dir+'power_gain_{:.2f}eff.png'.format(eff*100))
